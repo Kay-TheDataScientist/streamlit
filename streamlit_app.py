74be597cfed5469d96219df64a48228518f8e117
A. <abell87@charlotte.edu>
2025-04-21 14:07:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,9 +42,9 @@ subcategories = df1['Sub_Category'].unique()
 selected_subcategories = st.multiselect("Select Subcategories", subcategories)
 
 st.write("### (3) Show a line chart of sales for the selected items in (2)")
-df2 =df1[df1['Sub_Category'].isin(selected_subcategories)]
-sales_by_month1 = df2.groupby(['Sub_Category', pd.Grouper(freq='M')])['Sales'].sum().unstack(level=0)
+sales_by_month1 = df2.groupby(pd.Grouper(freq='M'))['Sales'].sum()
 st.line_chart(sales_by_month1)
+
 
 st.write("### (4) Show three metrics for the selected items in (2): total sales, total profit, and overall profit margin (%)")
 st.metric("Total Sales", df2['Sales'].sum())
